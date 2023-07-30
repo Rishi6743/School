35e5f478c95fbb13ad9651a0c380bcf8871ef567
Rishi Pradhang <pradhanrishi@gmail.com>
2023-07-30 16:37:44 +0530

    Logo Update

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -7,8 +7,8 @@ const LogoOnlyNavBar = () => {
       <Container className='d-flex justify-content-between align-items-center'>
         <Navbar.Brand className='text-white' >
           <img
-            src="http://davbsp.org/MyImages/LOGO.png"
-            alt="Your Logo"
+            src="/images/LOGO.png"
+            alt="Logo"
             style={{ height: '7rem' }}
           />
          D.A.V Public School
@@ -19,3 +19,4 @@ const LogoOnlyNavBar = () => {
 };
 
 export default LogoOnlyNavBar;
+
